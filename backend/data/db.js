132858ebf509c1819ef12850a0fcc789b49aee68
Shorkk <dashih@frba.utn.edu.ar>
2025-11-29 00:17:24 -0300

    correciones conexion mongoDB

--- backend/data/db.js
+++ backend/data/db.js
@@ -4,9 +4,14 @@ const strConnection = process.env.DB_URL
 
 exports.connectDB = async () => {
     if (!strConnection) {
-        throw new Error("La cadena de conexión a la base de datos no está definida en las variables de entorno.")
-
-        await mongoose.connect(strConnection)
-        console.log("Conexión a la base de datos exitosa")
+        throw new Error("La cadena de conexión a la base de datos no está definida en las variables de entorno.");
     }
-}
+
+    try {
+        await mongoose.connect(strConnection);
+        console.log("Conexión a la base de datos exitosa");
+    } catch (error) {
+        console.error("Error al conectar a la base de datos:", error);
+        throw error;
+    }
+};
