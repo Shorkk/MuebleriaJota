625081f19d24bcbcd5946929dd8e176222e8ffa7
Shorkk <dashih@frba.utn.edu.ar>
2025-11-29 16:12:48 -0300

    corrección al mostrar mi perfil

--- client/src/context/AppProvider.jsx
+++ client/src/context/AppProvider.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect, useCallback } from "react";
-import { fetchUsers, createUser } from "../data/db";
+import { fetchUsers, createUser, PerfilUsuario} from "../service/usersService";
 // import { fetchProductos } from "../services/productService;
+import { useAuthContext } from "./AuthContext";
 import { AppContext } from "./AppContext";
 
 
 export const AppProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
+  const [userActual, setUserActual] = useState()
+  const {isAuthenticated, token} = useAuthContext()
   // const [cartItems, setCartItems] = useState([]);
 
   // const cargarProductos = useCallback(async () => {
@@ -22,30 +25,49 @@ export const AppProvider = ({ children }) => {
   // }, [cargarProductos]);
 
   useEffect(() => {
-    const cargarUsers = async () => {
-      try {
-        const data = await fetchUsers();
-        console.log(data)
-        setUsers(data);
-      } catch (e) {
-        console.error(e);
-      }
-    };
-
     cargarUsers();
-  }, []);
+    if(isAuthenticated){
+      obtenerPerfilUsuario(token)
+    }else{
+      setUserActual(null)
+    }
+  }, [isAuthenticated,token]);
+  
+  const cargarUsers = async () => {
+    try {
+      const data = await fetchUsers();
+      // console.log(data)
+      setUsers(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
-const agregarUser = async (data) => {
-      const nuevo = await createUser(data);
-      setUsers((prev) => [...prev, nuevo]);
-      return nuevo;
+    const agregarUser = async (data) => {
+    const nuevo = await createUser(data);
+    setUsers((prev) => [...prev, nuevo]);
+    return nuevo;
+  };
+
+  const obtenerPerfilUsuario= async (token)=>{
+        //  const headers = getAuthHeaders()
+        // console.log(headers)
+        try{
+          const data = await PerfilUsuario(token)
+          console.log(data)
+          setUserActual(data)
+        }catch(err){
+          console.error("Error al obtener perfil:", err);
+        }
   }
 
   return (
     <AppContext.Provider
       value={{
         users,
-        agregarUser
+        userActual,
+        agregarUser,
+        obtenerPerfilUsuario
       }}
     >
       {children}
