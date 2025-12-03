701e78cc4162ff2e9bc30762a9a528da9d1c096e
Shorkk <dashih@frba.utn.edu.ar>
2025-12-03 18:13:01 -0300

    corrección nombre de rutas, eliminación de codigo js antiguo

--- client/src/service/productService.js
+++ client/src/service/productService.js
@@ -1,20 +1,20 @@
 const BASE_URL = import.meta.env.VITE_BASE_URL
 
 export async function fetchProducts() { // Me retorna todo los productos
-  const res = await fetch(`${BASE_URL}/api/producto`);
+  const res = await fetch(`${BASE_URL}/api/productos`);
 
   if (!res.ok) throw new Error('Error al obtener los productos');
   return res.json();
 }
 
 export async function fetchProductoPorId(id) { // Me retorna 1 producto
-  const res = await fetch(`${BASE_URL}/api/producto/${id}`);
+  const res = await fetch(`${BASE_URL}/api/productos/${id}`);
   if (!res.ok) throw new Error('Producto no encontrado');
   return res.json();
 }
 
 export async function createProducto(data) { // Me retorna el producto creado
-  const res = await fetch(`${BASE_URL}/api/producto`, {
+  const res = await fetch(`${BASE_URL}/api/productos`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
@@ -25,7 +25,7 @@ export async function createProducto(data) { // Me retorna el producto creado
 
 export async function DetalleProducto(id) {
         try {
-        const response = fetch(`${BASE_URL}/api/producto/${id}`, {
+        const response = fetch(`${BASE_URL}/api/productos/${id}`, {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
